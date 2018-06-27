680744ef63bd119fa0faaa0f2cd4ca2eecb086c1
Jason Derr <dharrmabum@gmail.com>
2018-06-27 00:29:39 -0700

    Saxalocity javascript working. all three column synced

--- js/saxalocity.js
+++ js/saxalocity.js
@@ -1,27 +1,30 @@
 function activateGallery() {
-let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-let mainImage = document.querySelector("#gallery-photo img");
-thumbnails.forEach(function(thumbnail){
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-video iframe");
+    thumbnails.forEach(function(thumbnail){
     //preload large images
-    largeVersion = new Image();
-    largeVersion.src = thumbnail.dataset.FILL_IN;
+        largeVersion = new Image();
+        largeVersion.src = thumbnail.dataset.FILL_IN;
 
-    thumbnail.addEventListener("click", function(){
+        thumbnail.addEventListener("click", function(){
         // code to set clicked image as amin image.
-        let newImageSrc = thumbnail.dataset.largeVersion;
-        mainImage.setAttribute("src", newImageSrc);
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            mainImage.setAttribute("src", newImageSrc);
 
         //change which image is current
-        document.querySelector(".current").classList.remove("current");
-        thumbnail.parentNode.classList.add("current");
+            document.querySelector(".current").classList.remove("current");
+            thumbnail.parentNode.classList.add("current");
 
         //Update image info
-        let galleryInfo = document.querySelector("#gallery-info");
-        let title       = galleryInfo.querySelector(".title");
-        let description = galleryInfo.querySelector(".description");
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title       = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
 
-        title.innerHTML       = thumbnail.dataset.title;
-        description.innerHTML = thumbnail.dataset.description;
+            title.innerHTML       = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
         });
     });
 }
+
+
+
